8059d3c9b94ae4db61eff4bd08cac433e7de9313
jhanny15 <jhannycortes@gmail.com>
2021-04-27 18:40:56 +0200

    Updated Polygon

--- src/geometry/polygons/base/Polygon.java
+++ src/geometry/polygons/base/Polygon.java
@@ -1,6 +1,25 @@
 package geometry.polygons.base;
 
 public class Polygon {
+    private int numSides = 3;
+    private double[] sides;
+
+    /**
+     * Costruttore di Poligono che crea di base un triangolo
+     */
+    public Polygon() {
+        this.sides = new double[numSides];
+    }
+
+    /**
+     * Costruttore di Poligono con il numero di lati specificati
+     * @param numSides numero di lati
+     */
+    public Polygon(int numSides) {
+        this.numSides = numSides;
+        this.sides = new double[numSides];
+    }
+
 
     public double area(){
         return 0;
